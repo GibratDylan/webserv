db821674fe27321766782a3ba102b0e264a6a5a2
sskobyak <scob@ya.ru>
2026-03-08 23:51:49 +0100

    refactor(server): Refine server session and request lifecycle

--- src/server/SessionManager.cpp
+++ src/server/SessionManager.cpp
@@ -1,16 +1,34 @@
 #include <sstream>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
 #include "SessionManager.h"
 #include "HttpRequest.h"
 #include "HttpResponse.h"
-
+#include "utils.h"
 
 std::string generateSessionId()
 {
-    std::stringstream ss;
-    ss << std::time(NULL) << rand();
-    return ss.str();
+	std::ostringstream ss;
+	srand(std::time(NULL) + rand());
+	ss << std::time(NULL) << rand() << std::clock();
+	return ss.str();
+}
+
+std::string extractSessionId(const std::string& cookies) {
+	std::string sessionKey = "session_id=";
+	size_t pos = cookies.find(sessionKey);
+	if (pos == std::string::npos)
+		return "";
+
+	pos += sessionKey.size();
+	size_t end = cookies.find(';', pos);
+	if (end == std::string::npos)
+		end = cookies.size();
+
+	return trim(cookies.substr(pos, end - pos));
 }
 
 SessionManager::SessionManager()
@@ -20,67 +38,59 @@ SessionManager::SessionManager()
 
 void SessionManager::setTtl(size_t ttl)
 {
-    _ttl = ttl;
+	_ttl = ttl;
 }
 
 std::string SessionManager::createSession()
 {
-    std::string id = generateSessionId();
-
-    Session s;
-    s.id = id;
-    s.last_access = std::time(NULL);
-
-    _sessions[id] = s;
-    return id;
+	std::string id = generateSessionId();
+	Session s;
+	s.id = id;
+	s.last_access = std::time(NULL);
+	_sessions[id] = s;
+	return id;
 }
 
 Session* SessionManager::getSession(const std::string& id)
 {
-    std::map<std::string, Session>::iterator it = _sessions.find(id);
+	std::map<std::string, Session>::iterator it = _sessions.find(id);
+	if (it == _sessions.end())
+		return NULL;
 
-    if (it == _sessions.end())
-        return NULL;
+	if (std::time(NULL) - it->second.last_access > (long)_ttl) {
+		_sessions.erase(it);
+		return NULL;
+	}
 
-    if (std::time(NULL) - it->second.last_access > (long)_ttl)
-    {
-        _sessions.erase(it);
-        return NULL;
-    }
-
-    it->second.last_access = std::time(NULL);
-    return &it->second;
+	it->second.last_access = std::time(NULL);
+	return &it->second;
 }
 
-
-std::string extractSessionId(const std::string& cookies)
-{
-    const std::string sessionKey = "session_id=";
-    size_t pos = cookies.find(sessionKey);
-    if (pos == std::string::npos)
-        return "";
-
-    pos += sessionKey.size();
-    size_t end = cookies.find(';', pos);
-
-    if (end == std::string::npos)
-        return cookies.substr(pos);
-
-    return cookies.substr(pos, end - pos);
+void SessionManager::cleanup() {
+	std::map<std::string, Session>::iterator it = _sessions.begin();
+	while (it != _sessions.end()) {
+		if (std::time(NULL) - it->second.last_access > (long)_ttl) {
+			_sessions.erase(it++);
+		} else {
+			++it;
+		}
+	}
 }
 
 void SessionManager::transferSession(HttpRequest *request, HttpResponse *response)
 {
-    std::string cookies = request->getHeader("Cookie");
+	std::string cookieHeader =  request->getHeader("Cookie");
 
-    std::string sessionId = extractSessionId(cookies);
+	std::string sessionId = extractSessionId(cookieHeader);
 
-    Session* session = getSession(sessionId);
+	Session* session = NULL;
+	if (!sessionId.empty()) {
+		session = getSession(sessionId);
+	}
 
-    if (!session)
-    {
-        std::string newId = createSession();
-        session = getSession(newId);
-        response->addHeader("Set-Cookie", "session_id=" + newId + "; Path=/; HttpOnly");
-    }
+	if (!session) {
+		sessionId = createSession();
+		session = getSession(sessionId);
+		response->addHeader("Set-Cookie", "session_id=" + sessionId + "; Path=/; HttpOnly; SameSite=Lax; Max-Age=3600");
+	}
 }
